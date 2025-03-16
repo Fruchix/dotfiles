a62b4d08d0e5f53209d88939cf5ff289310bcf88
Fruchix <fruchix@gmail.com>
2025-03-16 23:43:01 +0100

    feat: add first version of install script

--- bashrc
+++ bashrc
@@ -8,7 +8,7 @@ esac
 
 so() { for f in $@; do [ -s $1 ] && source "$f"; done; }
 
-script_dir="$(dirname "${BASH_SOURCE[0]}")"
+script_dir=$(dirname "$(readlink -f "$0")")
 
 so "${script_dir}/aliases.d"/*
 so "${script_dir}/completion.d"/*
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+script_dir=$(dirname "$(readlink -f "$0")")
+
+if grep -q Fruchix/dotfiles:install.sh "$HOME/.bashrc"; then
+    echo -e "File '$HOME/.bashrc' is already sourcing Fruchix's dotfiles with the following lines:\n"
+    grep --color=always Fruchix/dotfiles:install.sh "$HOME/.bashrc"
+
+    # echo -e "\nInstallation already done. Exiting process."
+    exit 1
+else
+    cat << EOF >> "$HOME/.bashrc"
+
+### Automaticaly added by Fruchix/dotfiles:install.sh ###
+source $script_dir/bashrc   ### Automaticaly added by Fruchix/dotfiles:install.sh
+### Automaticaly added by Fruchix/dotfiles:install.sh ###
+
+EOF
+
+    echo "Added sourcing of '$script_dir/bashrc' in '$HOME/.bashrc'"
+fi
+
+if [[ ! -d $HOME/.siu ]]; then
+    git clone https://github.com/Fruchix/SIU.git
+    pushd SIU
+    ./install.sh
+    popd
+
+    source $HOME/.bashrc
+
+    siu install -M
+fi
