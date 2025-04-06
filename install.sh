6b45280c73268ffc7ea84d7d822a75cc1dc59364
Fruchix <fruchix@gmail.com>
2025-04-06 23:58:08 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,30 +2,61 @@
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if grep -q Fruchix/dotfiles:install.sh "$HOME/.bashrc"; then
-    echo -e "File '$HOME/.bashrc' is already sourcing Fruchix's dotfiles with the following lines:\n"
-    grep --color=always Fruchix/dotfiles:install.sh "$HOME/.bashrc"
+setup_inputrc()
+{
+    if [[ -f "$HOME/.inputrc" ]]; then
+        mv "$HOME/.inputrc" "$HOME/.inputrc.bak.$(date '+%Y-%m-%d_%H:%M:%S')"
+    fi
 
-    # echo -e "\nInstallation already done. Exiting process."
-    exit 1
-else
-    cat << EOF >> "$HOME/.bashrc"
+    ln -s "${PWD}/inputrc" "$HOME/.inputrc"
+}
+
+setup_bashrc()
+{
+    if grep -q Fruchix/dotfiles:install.sh "$HOME/.bashrc"; then
+        echo -e "File '$HOME/.bashrc' is already sourcing Fruchix's dotfiles with the following lines:\n"
+        grep --color=always Fruchix/dotfiles:install.sh "$HOME/.bashrc"
+    else
+        cat << EOF >> "$HOME/.bashrc"
 
 ### Automaticaly added by Fruchix/dotfiles:install.sh ###
 source $script_dir/bashrc   ### Automaticaly added by Fruchix/dotfiles:install.sh
 ### Automaticaly added by Fruchix/dotfiles:install.sh ###
 
 EOF
+        echo "Added sourcing of '$script_dir/bashrc' in '$HOME/.bashrc'"
+    fi
+}
 
-    echo "Added sourcing of '$script_dir/bashrc' in '$HOME/.bashrc'"
-fi
+setup_zshrc()
+{
+    if grep -q Fruchix/dotfiles:install.sh "$HOME/.zshrc"; then
+        echo -e "File '$HOME/.zshrc' is already sourcing Fruchix's dotfiles with the following lines:\n"
+        grep --color=always Fruchix/dotfiles:install.sh "$HOME/.zshrc"
+    else
+        cat << EOF >> "$HOME/.zshrc"
 
-if [[ ! -d $HOME/.siu ]]; then
-    git clone https://github.com/Fruchix/SIU.git
-    pushd SIU
-    ./install.sh
-    popd
+### Automaticaly added by Fruchix/dotfiles:install.sh ###
+source $script_dir/zshrc   ### Automaticaly added by Fruchix/dotfiles:install.sh
+### Automaticaly added by Fruchix/dotfiles:install.sh ###
 
-    source $HOME/.siu/siu_bashrc
-    $HOME/.siu/bin/siu install -M
-fi
+EOF
+        echo "Added sourcing of '$script_dir/zshrc' in '$HOME/.zshrc'"
+    fi
+}
+
+setup_siu()
+{
+    if [[ ! -d $HOME/.siu ]]; then
+        git clone https://github.com/Fruchix/SIU.git
+        ./SIU/install
+
+        source $HOME/.siu/siu_bashrc
+        $HOME/.siu/bin/siu install -M
+    fi
+}
+
+setup_inputrc
+setup_bashrc
+setup_zshrc
+setup_siu
