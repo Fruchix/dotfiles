e4ba89156b25e290b687f4157e0f8308a9b5e21b
Fruchix <fruchix@gmail.com>
2026-01-20 16:21:27 +0100

    feat(utils_loader): add reload mode and --all option

--- utils_loader.sh
+++ utils_loader.sh
@@ -1,15 +1,15 @@
 DFLOADER() {
     if [[ $# -lt 1 ]]; then
-        echo "Usage: DFLOADER <load|unload|list> [UTILITY_NAME]..." >&2
+        echo "Usage: DFLOADER <load|unload|reload|list> [UTILITY_NAME [...]] [--all]" >&2
         return 1
     fi
 
     local mode="$1"
     case "$mode" in
-        load|unload|list) ;;
+        load|unload|reload|list) ;;
         *)
             echo "Invalid mode: $mode." >&2
-            echo "Usage: DFLOADER <load|unload|list> [UTILITY_NAME]..." >&2
+            echo "Usage: DFLOADER <load|unload|reload|list> [UTILITY_NAME [...]] [--all]" >&2
             return 1
             ;;
     esac
@@ -52,29 +52,34 @@ DFLOADER() {
     # load or unload modes only
 
     if [[ "$2" == "--all" ]]; then
-        local utils_list
-        utils_list="$(find "${utils_dir}" -maxdepth 1 -type f -exec basename {} \;)"
+        local utils_list=()
+        while IFS= read -r; do
+            utils_list+=("$REPLY")
+        done < <(find "${utils_dir}" -maxdepth 1 -type f -exec basename {} \;)
 
-        DFLOADER "$mode" $utils_list
+        DFLOADER "$mode" "${utils_list[@]}"
 
         return 0
     fi
 
+    local util_name_clean
+    local util_name
+    local util_file
+    local func_names
+    local alias_names
     while [[ "$#" -gt 1 ]]; do
         shift
-        local util_name="$1"
-        local utils_dir="${DOTFILES_HOME}/utils"
-        local util_file="${utils_dir}/${util_name}"
+        util_name="$1"
+        util_file="${utils_dir}/${util_name}"
 
         if [[ ! -e "${util_file}" ]]; then
-            echo "Utility '${util_name}' not found in ${utils_dir}." >&2
+            echo "Utility not found in ${utils_dir}: ${util_name}" >&2
             return 1
         fi
 
-        local util_name_clean
         util_name_clean=$(command echo "$util_name" | command tr ' +-.!?():,;=' '_' | command tr --complement --delete "a-zA-Z0-9_" | command tr '[:upper:]' '[:lower:]')
 
-        local func_names=()
+        func_names=()
         while IFS= read -r; do
             func_names+=("$REPLY")
         done < <(awk '
@@ -90,7 +95,7 @@ DFLOADER() {
 }
 ' "${util_file}" 2>/dev/null)
 
-        local alias_names=()
+        alias_names=()
         while IFS= read -r; do
             alias_names+=("$REPLY")
         done < <(awk '/^[[:space:]]*alias[[:space:]]+[A-Za-z_][A-Za-z0-9_]*=/ {
@@ -99,66 +104,72 @@ DFLOADER() {
     print;
 }' "${util_file}" 2>/dev/null)
 
-        if [[ "$mode" == "load" ]]; then
-            if [[ ":${_DF_LOADED_MODULES}:" == *":${util_name_clean}:"* ]]; then
-                echo "Utility '${util_name}' is already loaded."
-                continue
-            fi
+        case "$mode" in
+            reload)
+                DFLOADER "unload" "$util_name"
+                DFLOADER "load" "$util_name"
+                ;;
+            load)
+                if [[ ":${_DF_LOADED_MODULES}:" == *":${util_name_clean}:"* ]]; then
+                    echo "Utility already loaded: ${util_name}"
+                    continue
+                fi
 
-            echo "Loading utility: ${util_name}"
-            if [[ "${#func_names[@]}" -gt 0 ]]; then
-                echo "Functions:"
-                for func in "${func_names[@]}"; do
-                    echo " - $func"
-                done
-            fi
-            if [[ "${#alias_names[@]}" -gt 0 ]]; then
-                echo "Aliases:"
-                for alias in "${alias_names[@]}"; do
-                    echo " - $alias"
-                done
-            fi
-            . "${util_file}"
-            if [[ -n "${_DF_LOADED_MODULES}" ]]; then
-                export _DF_LOADED_MODULES="${_DF_LOADED_MODULES}:${util_name_clean}"
-            else
-                export _DF_LOADED_MODULES="${util_name_clean}"
-            fi
-        else
-            if [[ ":${_DF_LOADED_MODULES}:" != *":${util_name_clean}:"* ]]; then
-                echo "Utility '${util_name}' is not loaded."
-                continue
-            fi
+                echo "Loading utility: ${util_name}"
+                if [[ "${#func_names[@]}" -gt 0 ]]; then
+                    echo "Functions:"
+                    for func in "${func_names[@]}"; do
+                        echo " - $func"
+                    done
+                fi
+                if [[ "${#alias_names[@]}" -gt 0 ]]; then
+                    echo "Aliases:"
+                    for alias in "${alias_names[@]}"; do
+                        echo " - $alias"
+                    done
+                fi
+                . "${util_file}"
+                if [[ -n "${_DF_LOADED_MODULES}" ]]; then
+                    export _DF_LOADED_MODULES="${_DF_LOADED_MODULES}:${util_name_clean}"
+                else
+                    export _DF_LOADED_MODULES="${util_name_clean}"
+                fi
+                ;;
+            unload)
+                if [[ ":${_DF_LOADED_MODULES}:" != *":${util_name_clean}:"* ]]; then
+                    echo "Utility not loaded: ${util_name}"
+                    continue
+                fi
 
-            echo "Unloading utility: ${util_name}"
-            if [[ -n "${func_names[*]}" ]]; then
-                echo "Functions:"
-                for func in "${func_names[@]}"; do
-                    unset -f "$func"
-                    echo " - $func"
-                done
-            fi
-            if [[ -n "${alias_names[*]}" ]]; then
-                echo "Aliases:"
-                for alias in "${alias_names[@]}"; do
-                    unalias "$alias"
-                    echo " - $alias"
-                done
-            fi
-            _DF_LOADED_MODULES=":${_DF_LOADED_MODULES}:"
-            _DF_LOADED_MODULES=${_DF_LOADED_MODULES//:"$util_name_clean":/:}
-            _DF_LOADED_MODULES=${_DF_LOADED_MODULES#:}
-            _DF_LOADED_MODULES=${_DF_LOADED_MODULES%:}
-            export _DF_LOADED_MODULES
-        fi
-
+                echo "Unloading utility: ${util_name}"
+                if [[ -n "${func_names[*]}" ]]; then
+                    echo "Functions:"
+                    for func in "${func_names[@]}"; do
+                        unset -f "$func"
+                        echo " - $func"
+                    done
+                fi
+                if [[ -n "${alias_names[*]}" ]]; then
+                    echo "Aliases:"
+                    for alias in "${alias_names[@]}"; do
+                        unalias "$alias"
+                        echo " - $alias"
+                    done
+                fi
+                _DF_LOADED_MODULES=":${_DF_LOADED_MODULES}:"
+                _DF_LOADED_MODULES=${_DF_LOADED_MODULES//:"$util_name_clean":/:}
+                _DF_LOADED_MODULES=${_DF_LOADED_MODULES#:}
+                _DF_LOADED_MODULES=${_DF_LOADED_MODULES%:}
+                export _DF_LOADED_MODULES
+                ;;
+        esac
         echo ""
     done
 }
 
 _complete_DFLOADER() {
     local cur_word="${COMP_WORDS[COMP_CWORD]}"
-    local commands="load unload list"
+    local commands="load unload reload list"
 
     if [[ ${COMP_CWORD} -eq 1 ]]; then
         COMPREPLY=( $(compgen -W "${commands}" -- "${cur_word}") )
