3755fc18aa6592d8a27adaf1a41a5811dccb4073
Mélian Fruchart <76095898+Fruchix@users.noreply.github.com>
2026-04-30 11:18:24 +0200

    Create setup-code.sh

---
+++ setup/setup-code.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+install_extensions() {
+    local extensions
+    extensions=(
+        "catppuccin.catppuccin-vsc"
+        "mads-hartmann.bash-ide-vscode"
+        "ms-vscode.remote-explorer"
+        "ms-vscode-remote.remote-ssh"
+        "ms-vscode-remote.remote-ssh-edit"
+        "ms-vscode.cpptools-themes"
+        "yzhang.markdown-all-in-one"
+        "timonwong.shellcheck"
+        "mathematic.vscode-latex"
+        "james-yu.latex-workshop"
+        "github.copilot-chat"
+        "github.copilot-chat"
+    )
+
+    echo "Installing extensions:"
+    local ext
+    for ext in "${extensions[@]}"; do
+        echo "- $ext"
+        code --install-extension "$ext" --force
+    done
+    echo "All extensions installed."
+}
+
+echo "VS Code Setup"
+
+install_extensions
